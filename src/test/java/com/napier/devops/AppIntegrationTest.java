277e78f6c966a8f769d320abf46c2c97ca311e56
Nyeinsu-enu <40804619@live.napier.ac.uk>
2025-11-06 13:26:57 +0630

    Add Integration Test

--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -2,24 +2,23 @@ package com.napier.devops;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppIntegrationTest
-{
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AppIntegrationTest {
     static App app;
 
     @BeforeAll
-    static void init()
-    {
+    void init() {
         app = new App();
-        // Use Docker service name instead of localhost
-        app.connect("db:33060", 30000);
+        // Use container hostname and port 3306
+        app.connect("db:3306", 30000);
     }
 
     @Test
-    void testGetEmployee()
-    {
+    void testGetEmployee() {
         Employee emp = app.getEmployee(255530);
         assertNotNull(emp, "Employee should not be null");
         assertEquals(255530, emp.emp_no);
